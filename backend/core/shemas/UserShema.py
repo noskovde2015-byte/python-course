f8e17e29531ea48aa181c51b610b9cd4e6fe5a75
Danil <noskovde2015@gmail.com>
2026-03-31 11:01:22 +0300

    Create User shema

---
+++ backend/core/shemas/UserShema.py
@@ -0,0 +1,21 @@
+import datetime
+
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from core.models.user import UserRole
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    nickname: str = Field(max_length=50)
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    is_active: bool
+    created_at: datetime.datetime
+    role: UserRole
