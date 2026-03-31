501f283a4983f8c1218626788259f7f11b0e2266
Danil <noskovde2015@gmail.com>
2026-03-31 11:00:47 +0300

    Add api router and include it in main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 from core.config import settings
+from api import router as api_router
 
 app = FastAPI()
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port ,reload=True)
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
