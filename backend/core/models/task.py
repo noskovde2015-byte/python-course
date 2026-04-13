68e9fd06ee04697e5f071622542d35120d851719
Danil <noskovde2015@gmail.com>
2026-04-13 11:26:31 +0300

    Update task table

--- backend/core/models/task.py
+++ backend/core/models/task.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Text, ForeignKey, String
+from sqlalchemy import Text, ForeignKey, String, JSON, Boolean
 from core.models import Base
 from typing import TYPE_CHECKING
 
@@ -11,7 +11,17 @@ class Task(Base):
     __tablename__ = "tasks"
 
     question: Mapped[str] = mapped_column(Text, nullable=False)
-    answer: Mapped[str] = mapped_column(Text, nullable=False)
+
+    answer: Mapped[str | None] = mapped_column(Text, nullable=True)
+
+    # варианты ответа
+    options: Mapped[list[str] | None] = mapped_column(JSON, nullable=True)
+
+    # правильные ответы
+    correct_answers: Mapped[list[str] | None] = mapped_column(JSON, nullable=True)
+
+    multiple: Mapped[bool] = mapped_column(Boolean, default=False)
+
     type: Mapped[str] = mapped_column(String(20), default="quiz", nullable=False)
 
     lesson_id: Mapped[int] = mapped_column(ForeignKey("lessons.id", ondelete="CASCADE"))
